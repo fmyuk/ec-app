dc533bda2fe2591a1628ee66275830f7061cb00a
fumi uk <dcfumiya.238@gmail.com>
2021-01-28 18:07:40 +0900

    ec-app

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router";
-import { LoginComponent } from "./containers";
-import { Login, Home } from "./templates";
+import { Login, Home, SignUp } from "./templates";
 
 const Router = () => {
   return (
     <Switch>
+      <Route exact path={"/signup"} component={SignUp} />
       <Route exact path={"/login"} component={Login} />
-      <Route exact path={"/logcom"} component={LoginComponent} />
       <Route exact path={"(/)?"} component={Home} />
     </Switch>
   );
---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlc2LUCFC8BQnSIZ309cIjrLn9tsZdUjQ",
+  authDomain: "ec-app-9dbe4.firebaseapp.com",
+  projectId: "ec-app-9dbe4",
+  storageBucket: "ec-app-9dbe4.appspot.com",
+  messagingSenderId: "197453846019",
+  appId: "1:197453846019:web:84326def1359a5e4112036",
+  measurementId: "G-H1YSRC34FQ"
+};
